b0eae9372babee592bccd42c47a9912658971812
shaheen7a <aousshaheen7@gmail.com>
2024-01-23 21:46:11 +0400

    date picker

--- components/ui/shared/EventForm.tsx
+++ components/ui/shared/EventForm.tsx
@@ -20,6 +20,10 @@ import { eventDefaultValues } from "@/constants";
 import { Textarea } from "../textarea";
 import { FileUploader } from "./FileUploader";
 import { useState } from "react";
+import Image from "next/image";
+import Dropdown from "./Dropdown";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 type EventFormProps = {
   userId: string;
@@ -28,6 +32,7 @@ type EventFormProps = {
 
 const EventForm = ({ userId, type }: EventFormProps) => {
   const [files, setFiles] = useState<File[]>([]);
+  // const [startDate, setStartDate] = useState(new Date());
   const initialValues = eventDefaultValues;
 
   // 1. Define your form.
@@ -72,10 +77,10 @@ const EventForm = ({ userId, type }: EventFormProps) => {
             render={({ field }) => (
               <FormItem className="w-full">
                 <FormControl>
-                  {/* <Dropdown
+                  <Dropdown
                     onChangeHandler={field.onChange}
                     value={field.value}
-                  /> */}
+                  />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -126,12 +131,12 @@ const EventForm = ({ userId, type }: EventFormProps) => {
               <FormItem className="w-full">
                 <FormControl>
                   <div className="flex-center h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
-                    {/* <Image
+                    <Image
                       src="/assets/icons/location-grey.svg"
                       alt="calendar"
                       width={24}
                       height={24}
-                    /> */}
+                    />
 
                     <Input
                       placeholder="Event location or Online"
@@ -154,24 +159,24 @@ const EventForm = ({ userId, type }: EventFormProps) => {
               <FormItem className="w-full">
                 <FormControl>
                   <div className="flex-center h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
-                    {/* <Image
+                    <Image
                       src="/assets/icons/calendar.svg"
                       alt="calendar"
                       width={24}
                       height={24}
                       className="filter-grey"
-                    /> */}
+                    />
                     <p className="ml-3 whitespace-nowrap text-grey-600">
                       Start Date:
                     </p>
-                    {/* <DatePicker
+                    <DatePicker
                       selected={field.value}
                       onChange={(date: Date) => field.onChange(date)}
                       showTimeSelect
                       timeInputLabel="Time:"
                       dateFormat="MM/dd/yyyy h:mm aa"
                       wrapperClassName="datePicker"
-                    /> */}
+                    />
                   </div>
                 </FormControl>
                 <FormMessage />
@@ -186,24 +191,24 @@ const EventForm = ({ userId, type }: EventFormProps) => {
               <FormItem className="w-full">
                 <FormControl>
                   <div className="flex-center h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
-                    {/* <Image
+                    <Image
                       src="/assets/icons/calendar.svg"
                       alt="calendar"
                       width={24}
                       height={24}
                       className="filter-grey"
-                    /> */}
+                    />
                     <p className="ml-3 whitespace-nowrap text-grey-600">
                       End Date:
                     </p>
-                    {/* <DatePicker
+                    <DatePicker
                       selected={field.value}
                       onChange={(date: Date) => field.onChange(date)}
                       showTimeSelect
                       timeInputLabel="Time:"
                       dateFormat="MM/dd/yyyy h:mm aa"
                       wrapperClassName="datePicker"
-                    /> */}
+                    />
                   </div>
                 </FormControl>
                 <FormMessage />
@@ -220,13 +225,13 @@ const EventForm = ({ userId, type }: EventFormProps) => {
               <FormItem className="w-full">
                 <FormControl>
                   <div className="flex-center h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
-                    {/* <Image
+                    <Image
                       src="/assets/icons/dollar.svg"
                       alt="dollar"
                       width={24}
                       height={24}
                       className="filter-grey"
-                    /> */}
+                    />
                     <Input
                       type="number"
                       placeholder="Price"
@@ -271,12 +276,12 @@ const EventForm = ({ userId, type }: EventFormProps) => {
               <FormItem className="w-full">
                 <FormControl>
                   <div className="flex-center h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
-                    {/* <Image
+                    <Image
                       src="/assets/icons/link.svg"
                       alt="link"
                       width={24}
                       height={24}
-                    /> */}
+                    />
 
                     <Input
                       placeholder="URL"
